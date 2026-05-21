13ff9406fc24bc5d04130edfc964176970f6134c
Vae29 <valesalvalen@gmail.com>
2026-05-21 01:02:51 -0500

    validacion de datos del login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import pool, { testConnection } from './config/db.js';
+import authRoutes from './routes/auth.js';
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 testConnection();
+
+// Rutas
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
