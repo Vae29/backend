7b5c63735f14bc56a46c827e9304a1ee205bb333
Vae29 <valesalvalen@gmail.com>
2026-06-09 01:19:16 -0500

    prueba

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
       'http://localhost:3000',
       'http://127.0.0.1:5173',
       'http://127.0.0.1:5174',
-      'https://agrogestion-modulo-costos.netlify.app',
+      'https://agrogestion-modulo-costos.netlify.app/',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
