bcb367ed086709c1268949427bd17c4aadcc1ca9
Vae29 <valesalvalen@gmail.com>
2026-06-09 02:34:52 -0500

    prueba7

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     // Refleja el Origin que llegue desde el navegador.
     // Con credentials=true, esto evita mismatches por variantes exactas del Origin.
-    origin: true,
+    origin: 'https://agrogestion-modulo-costos.netlify.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
