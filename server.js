f59de77eec08b1ad7843a60ce579ba2c63f7bc53
Vae29 <valesalvalen@gmail.com>
2026-06-09 01:23:14 -0500

    prueba

--- server.js
+++ server.js
@@ -23,14 +23,7 @@ const PORT = process.env.PORT || 3000;
 // CORS
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:3000',
-      'http://127.0.0.1:5173',
-      'http://127.0.0.1:5174',
-      'https://agrogestion-modulo-costos.netlify.app/',
-    ],
+    origin:true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
