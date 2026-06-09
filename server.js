23945525a11d8ca1209d89e364d26f1a7d1e61d1
Vae29 <valesalvalen@gmail.com>
2026-06-09 01:13:16 -0500

    Agregar dominio Netlify a CORS

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(
       'http://localhost:3000',
       'http://127.0.0.1:5173',
       'http://127.0.0.1:5174',
+      'https://agrogestion-modulo-costos.netlify.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
